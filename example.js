ae00f0c749384d198c41f3f8da525d69a2cf5cf6
Albertinin Mourato <ams11@cin.ufpe.br>
2019-10-07 09:16:19 -0300

    Add prettier/lint configs

--- example.js
+++ example.js
@@ -1,9 +1,10 @@
-  const buildTable = require('./table-builder');
-  const headers = [
-    {dataKey: 'key1', name: 'Value 1'},
-    {dataKey: 'memory', name: 'Value 2'},
-    {dataKey: 'imageHint', name: 'Value 4'},
-  ];
+const buildTable = require('./table-builder');
+
+const headers = [
+  {dataKey: 'key1', name: 'Value 1'},
+  {dataKey: 'memory', name: 'Value 2'},
+  {dataKey: 'imageHint', name: 'Value 4'},
+];
 
 const data = [
   {
